e009b44ad118b8f6943e5c92cb8fc8e29ac1b98f
calebeLAR <ca6ele@gmail.com>
2023-10-19 11:59:22 -0300

    req03:feat: cria serializers e viewset para model project

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Project
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         fields = ["id", "name", "github", "linkedin", "bio"]
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    # essa linha, server para fazer a API receber no campo profile apenas o ID
+    # do perfil correspondente
+    profile = serializers.PrimaryKeyRelatedField(
+        queryset=Profile.objects.all()
+    )
+
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "name",
+            "description",
+            "github_url",
+            "keyword",
+            "key_skill",
+            "profile",
+        ]
