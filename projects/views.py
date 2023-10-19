a17eec40d1b0559ea95631d2e9b8e61d695ef132
calebeLAR <ca6ele@gmail.com>
2023-10-19 16:15:16 -0300

    req04:feat: costumiza viewsets de Profile

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,34 @@
 from rest_framework import viewsets
+from django.shortcuts import get_object_or_404, render
 from .models import Profile, Project
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import ProfileSerializer, ProjectSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile_id = kwargs.get("pk")
+            profile = get_object_or_404(Profile, pk=profile_id)
+            projects = profile.projects.all()
+            context = {
+                "profile": profile,
+                "projects": projects,
+                }
+            return render(
+                request,
+                "profile_detail.html",
+                context=context,
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
