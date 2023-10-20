97bbc597895509ca0ec398185467cef12d6c3d54
calebeLAR <ca6ele@gmail.com>
2023-10-20 11:57:06 -0300

    req05:feat: cria rota /certificates/ e  /certifying-institutions/ com o CRUD NORMAL

--- super_portfolio/urls.py
+++ super_portfolio/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from projects.views import ProfileViewSet, ProjectViewSet
+from projects.views import (
+    ProfileViewSet,
+    ProjectViewSet,
+    CertificateViewSet,
+    CertifyingInstitutionViewSet,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,8 +30,10 @@ from rest_framework_simplejwt.views import (
 )
 
 router = routers.DefaultRouter()
-router.register(r'profiles', ProfileViewSet)
-router.register(r'projects', ProjectViewSet)
+router.register(r"profiles", ProfileViewSet)
+router.register(r"projects", ProjectViewSet)
+router.register(r"certificates", CertificateViewSet)
+router.register(r"certifying-institutions", CertifyingInstitutionViewSet)
 # as rotas r'rota' ja inclue dentro dela estas:
 #       GET rota/
 #       POST rota/<id_do_perfil>
@@ -38,5 +45,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
