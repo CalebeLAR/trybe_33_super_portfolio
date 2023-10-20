e4ee622e7d3d162be63a38ac537749580bb104c0
calebeLAR <ca6ele@gmail.com>
2023-10-20 15:36:36 -0300

    req05:feat: modifica serializers para ter um CRUD inline

--- projects/serializers.py
+++ projects/serializers.py
@@ -28,16 +28,7 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class CertifyingInstitutionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = CertifyingInstitution
-        fields = ["id", "name", "url"]
-
-
-class CertificateSerializer(serializers.HyperlinkedModelSerializer):
-    certifying_institution = CertifyingInstitutionSerializer
-    profiles = ProfileSerializer
-
+class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
         fields = [
@@ -47,3 +38,31 @@ class CertificateSerializer(serializers.HyperlinkedModelSerializer):
             "timestamp",
             "profiles",
         ]
+
+
+class NestedCertificatesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ["id", "name", "timestamp"]
+
+
+class CertifyingInstitutionSerializer(serializers.ModelSerializer):
+    certificates = NestedCertificatesSerializer(many=True)
+
+    class Meta:
+        model = CertifyingInstitution
+        fields = ["id", "name", "url", "certificates"]
+
+    def create(self, validated_data):
+        certificates_data = validated_data.pop("certificates")
+        new_certifying_institution = CertifyingInstitution.objects.create(
+            **validated_data
+        )
+        new_certificate = {}
+        for certificate in certificates_data:
+            new_certificate = {
+                "name": certificate["name"],
+                "certifying_institution": new_certifying_institution,
+            }
+            CertificateSerializer().create(new_certificate)
+        return new_certifying_institution
